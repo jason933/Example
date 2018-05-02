c955a49b243f858c8b16424c6ced794622a98ff7
Hojune <jason93333@gmail.com>
2018-05-02 15:45:58 +0900

    LED_SHIFT_0.3

---
+++ LEDTEST/LEDTEST/main.cpp
@@ -0,0 +1,25 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+	
+int main(void)
+{
+	DDRD = 0xff;
+	PORTD = 0xff;
+
+	int i;	
+    /* Replace with your application code */
+    while (1) 
+    {
+		for (int i=0; i<7; i++)
+		{
+			PORTD = ~(1 << i);
+			_delay_ms(300);
+		}
+		for (int i=0; i<7; i++)
+		{
+			PORTD = ~(0x80 >> i);
+			_delay_ms(300);
+		}
+    }
+}
